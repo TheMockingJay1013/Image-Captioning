b28007dc347c329ab19a50a8bd14e5f4799de827
TheMockingJay1013 <navaneeth1013@gmail.com>
2023-02-11 11:55:42 +0530

    fixed_main.py

--- main.py
+++ main.py
@@ -38,15 +38,36 @@ def experiment(annotation_file, captioner, transforms, outputs):
 
     #Transform the required image (roll number mod 10) and save it seperately
     img=dset.__transformitem__(path)
-    out_path="./data/output/"+data["file_name"]
-    img=img.save(out_path)
+    
+    img=img.save(outputs)
     #Get the predictions from the captioner for the above saved transformed image  
-    caption=captioner(out_path,2)
+    caption=captioner(outputs,2)
     print(caption)
 
 def main():
     captioner = ImageCaptioningModel()
-    experiment('./data/annotations.jsonl', captioner, [FlipImage(), BlurImage(1)], None) # Sample arguments to call experiment()
+    experiment('./data/annotations.jsonl', captioner, None, "./data/output/2_A.jpg") # Sample arguments to call experiment()
+    print("First Experiment is Completed")
+    experiment('./data/annotations.jsonl', captioner, [FlipImage('horizontal')], "./data/output/2_B.jpg")
+    print("Second Experiment is Completed")
+    experiment('./data/annotations.jsonl', captioner, [BlurImage(20)], "./data/output/2_C.jpg")
+    print("Third Experiment is Completed")
+    
+    img=Image.open("./data/imgs/2.jpg")
+    size=img.size
+    nsize=(2*size[0],2*size[1])
+
+
+    experiment('./data/annotations.jsonl', captioner, [RescaleImage(nsize)], "./data/output/2_D.jpg")
+    print("Fourth Experiment is Completed")
+
+    msize=(int(0.5*size[0]),int(0.5*size[1]))
+    experiment('./data/annotations.jsonl', captioner, [RescaleImage(msize)], "./data/output/2_E.jpg")
+    print("Fifth Experiment is Completed")
+    experiment('./data/annotations.jsonl', captioner, [RotateImage(-90)], "./data/output/2_F.jpg")
+    print("Sixth Experiment is Completed")
+    experiment('./data/annotations.jsonl', captioner, [RotateImage(45)], "./data/output/2_G.jpg")
+    print("Seventh Experiment is Completed")
 
 
 if __name__ == '__main__':
